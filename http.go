27a6f4dfb0bfe16ca8fe90144f634aa6e5cbd48c
Ox1O5 <mushan65535@outlook.com>
2020-06-12 11:35:55 +0800

    a bug need to fix

--- http.go
+++ http.go
@@ -2,27 +2,36 @@ package gas
 
 import (
 	"fmt"
+	"github.com/Ox1O5/gas/consistenthash"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
 	"strings"
+	"sync"
 )
 
-const defaultBasePaht = "/_gascache/"
+const (
+	defaultBasePath = "/_gascache/"
+	defaultReplicas = 50
+
+)
 
 //HTTPPool implements PeerPicker for a pool of HTTP peers
 type HTTPPool struct {
 	// this peer's base URL, e.g. "https://example.net:8000"
 	self     string
 	basePath string
+	mu sync.Mutex
+	peers *consistenthash.Map
+	httpGetter map[string]*httpGetter
 }
 
 //NewHTTPPool initializes an HTTP Pool of peers.
 func NewHTTPPool(self string) *HTTPPool {
 	return &HTTPPool{
 		self:     self,
-		basePath: defaultBasePaht,
+		basePath: defaultBasePath,
 	}
 }
 
@@ -37,14 +46,15 @@ func (p *HTTPPool) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		panic("HTTPPool serving unexpected: " + r.URL.Path)
 	}
 	p.Log("%s %s", r.Method, r.URL.Path)
-	//<basePath>/<groupName>/<key> required
+	// /<basePath>/<groupName>/<key> required
 	parts := strings.SplitN(r.URL.Path[len(p.basePath):], "/", 2)
 	if len(parts) != 2 {
 		http.Error(w, "bad request", http.StatusBadRequest)
 		return
 	}
 
-	groupName, key := parts[0], parts[1]
+	groupName := parts[0]
+	key := parts[1]
 	group := GetGroup(groupName)
 	if group == nil {
 		http.Error(w, "no such group: "+groupName, http.StatusNotFound)
@@ -67,7 +77,7 @@ type httpGetter struct {
 
 func (h *httpGetter) Get (group string, key string) ([]byte, error) {
 	u := fmt.Sprintf(
-		"v%v%v%v%",
+		"%v%v/%v",
 		h.baseURL,
 		url.QueryEscape(group),
 		url.QueryEscape(key),
@@ -89,3 +99,29 @@ func (h *httpGetter) Get (group string, key string) ([]byte, error) {
 }
 
 var _PeerGetter = (*httpGetter)(nil)
+
+//Set updates the pool's peers
+func (p *HTTPPool)Set(peers...string)  {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	p.peers = consistenthash.NewMap(defaultReplicas, nil)
+	p.peers.Add(peers...)
+	p.httpGetter = make(map[string]*httpGetter, len(peers))
+	for _, peer := range peers{
+		p.httpGetter[peer] = &httpGetter{baseURL: peer + p.basePath}
+	}
+}
+
+
+//PickPeer picks a peer according to key
+func (p *HTTPPool)PickPeer(key string) (PeerGetter, bool) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	if peer := p.peers.Get(key) ; peer != "" || peer != p.self {
+		p.Log("Pick peer %s", peer)
+		return p.httpGetter[peer], true
+	}
+	return nil, false
+}
+
+var _PickPeer = (*PeerPicker)(nil)
---
+++ main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/Ox1O5/gas"
+	"log"
+	"net/http"
+)
+
+var db = map[string]string{
+	"Tom":  "630",
+	"Jack": "589",
+	"Sam":  "567",
+}
+
+func createGroup() *gas.Group  {
+	return gas.NewGroup("scores", 2<<10, gas.GetterFunc(
+		func(key string) ([]byte, error) {
+			log.Println("[SlowDB] search key", key)
+			if v, ok := db[key]; ok {
+				return []byte(v), nil
+			}
+			return nil, fmt.Errorf("%s not exist", key)
+		}))
+}
+
+func startCacheServer(addr string, addrs[]string, g *gas.Group)  {
+	peers := gas.NewHTTPPool(addr)
+	peers.Set(addrs...)
+	g.RegisterPeers(peers)
+	log.Println("GasCache is running at", addr)
+	log.Fatal(http.ListenAndServe(addr[7:], peers))
+}
+
+func startAPIServer(apiAddr string, g *gas.Group)  {
+	http.Handle("/api", http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			key := r.URL.Query().Get("key")
+			view, err := g.Get(key)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			w.Header().Set("Content-Type", "application/octet-stream")
+			w.Write(view.ByteSlice())
+		}))
+	log.Println("fontend server is running at", apiAddr)
+	log.Fatal(http.ListenAndServe(apiAddr[7:], nil))
+}
+
+
+func main() {
+	var port int
+	var api bool
+	flag.IntVar(&port, "port", 8001, "Gascache Server port")
+	flag.BoolVar(&api, "api", false, "Start a api server?")
+	flag.Parse()
+
+	apiAddr := "http://localhost:9797"
+	addrMap := map[int]string {
+		8001: "http://localhost:8001",
+		8002: "http://localhost:8002",
+		8003: "http://localhost:8003",
+	}
+
+	var addrs[]string
+	for _,v := range addrMap{
+		addrs = append(addrs, v)
+	}
+
+	g := createGroup()
+
+	if api {
+		go startAPIServer(apiAddr, g)
+	}
+	startCacheServer(addrMap[port], []string(addrs),g)
+}
